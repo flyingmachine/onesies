31720d60109da4360f4e3d18b6402eb2c506f558
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2010-02-19 22:23:30 -0500

    refactoring

--- electric_slide/javascripts/application.js
+++ electric_slide/javascripts/application.js
@@ -1,56 +1,75 @@
 $(function(){
-  var maxHeight = 0;
-  var maxTopMargin = 0;
-  var maxBottomMargin = 0;
-  var maxTopPadding = 0;
-  var maxBottomPadding = 0;
-  var maxTopBorder = 0;
-  var maxBottomBorder = 0;
+  // // I'm not sure this is even necessary...
+  // var maxHeight = 0;
+  // var maxTopMargin = 0;
+  // var maxBottomMargin = 0;
+  // var maxTopPadding = 0;
+  // var maxBottomPadding = 0;
+  // var maxTopBorder = 0;
+  // var maxBottomBorder = 0;
+  // function findMaxDimensions(slideElem) {
+  //   var height = $(this).height();
+  //   if(height > maxHeight) maxHeight = height;
+  //   
+  //   // margins
+  //   var margins = $(this).margin();
+  //   
+  //   var topMargin = margins.top;
+  //   if(topMargin > maxTopMargin) maxTopMargin = topMargin;
+  //   
+  //   var bottomMargin = margins.bottom;
+  //   if(bottomMargin > maxBottomMargin) maxBottomMargin = bottomMargin;
+  //   
+  //   // padding
+  //   var padding = $(this).padding();
+  //   
+  //   var topPadding = padding.top;
+  //   if(topPadding > maxTopPadding) maxTopPadding = topPadding;
+  //   
+  //   var bottomPadding = padding.bottom;
+  //   if(bottomPadding > maxBottomPadding) maxBottomPadding = bottomPadding;
+  //   
+  //   
+  //   // border
+  //   var border = $(this).border();
+  //   
+  //   var topBorder = border.top;
+  //   if(topBorder > maxTopBorder) maxTopBorder = topBorder;
+  //   
+  //   var bottomBorder = border.bottom;
+  //   if(bottomBorder > maxBottomBorder) maxBottomBorder = bottomBorder;
+  // }
+  // $("#slides").height(maxHeight + maxTopMargin + maxBottomMargin + maxTopPadding + maxBottomPadding + maxTopBorder + maxBottomBorder)
   
-  var width = $("#slides").width()
+  var slideIdentifier = ".slide";
+  var slideContainerIdentifier = "#slides";
+  var shouldInsertHeader = true;
+  var currentSlide = 0;
+  var slides = $(slideIdentifier);
+  var slideContainer = $(slideContainerIdentifier);
   
-  $(".slide").each(function(){
-    var height = $(this).height();
-    if(height > maxHeight) maxHeight = height;
-    
-    // margins
-    var margins = $(this).margin();
-    
-    var topMargin = margins.top;
-    if(topMargin > maxTopMargin) maxTopMargin = topMargin;
-    
-    var bottomMargin = margins.bottom;
-    if(bottomMargin > maxBottomMargin) maxBottomMargin = bottomMargin;
-    
-    // padding
-    var padding = $(this).padding();
-    
-    var topPadding = padding.top;
-    if(topPadding > maxTopPadding) maxTopPadding = topPadding;
-    
-    var bottomPadding = padding.bottom;
-    if(bottomPadding > maxBottomPadding) maxBottomPadding = bottomPadding;
-    
-    
-    // border
-    var border = $(this).border();
-    
-    var topBorder = border.top;
-    if(topBorder > maxTopBorder) maxTopBorder = topBorder;
-    
-    var bottomBorder = border.bottom;
-    if(bottomBorder > maxBottomBorder) maxBottomBorder = bottomBorder;
-  })
+  function slideWidth() {
+    return $("#slides").width()
+  }
   
-  $("#slides").height(maxHeight + maxTopMargin + maxBottomMargin + maxTopPadding + maxBottomPadding + maxTopBorder + maxBottomBorder)
+  function insertHeader(slideElem){
+  }
   
-  $(".slide").each(function(){
-    $(this).height(maxHeight)
+  // setup slides
+  slides.each(function(i){
+    if(shouldInsertHeader) insertHeader(this);
+    $(this).width(slideWidth());
     $(this).show();
-    $(this).width(width);
+    this.slidePosition = i;
   })
+  
+  // Navigation
+  function showSlide(slidePosition) {
+    
+  }
+  
   jQuery.easing.def = "easeOutQuart";
-  $("#slides").click(function(){
-    $('#track').animate({ marginLeft: '-' + width + 'px' }, 200);
+  slideContainer.click(function(){
+    $('#track').animate({ marginLeft: '-' + slideWidth() + 'px' }, 200);
   })
 })

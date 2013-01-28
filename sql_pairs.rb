e8dbd3ac397ce7bc570fe07f6f579c6bff2b578e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-01-28 11:48:03 -0500

    tweaks

--- sql_pairs.rb
+++ sql_pairs.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
+
+# Simple, not robust script for pairing sql fields to values
+
 sql = STDIN.read
 # sql = %{INSERT INTO "users" ("username", "email") VALUES ('joe schmoe', 'joe@schmoe.com')}
 
 begin
   fields = /\((.*?)\)/.match(sql)[1].split(",")
   values = /values \((.*?)\)/i.match(sql)[1].split(",")
-rescue
+  0.upto(fields.size) do |i|
+    puts "#{fields[i]} => #{values[i]}"
+  end
+rescue => e
   puts "Could not find pairs for string: #{sql}"
-end
-
-0.upto(fields.size) do |i|
-  puts "#{fields[i]} => #{values[i]}"
+  raise e
 end
